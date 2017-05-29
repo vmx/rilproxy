513a85cdd10f12d6b0963af00073796796e60e92
Volker Mische <volker.mische@gmail.com>
2017-05-29 23:32:13 +0200

    Make `swichUser()` compile again

--- src/b2g-dialer-forward.cpp
+++ src/b2g-dialer-forward.cpp
@@ -8,8 +8,9 @@
 #include <errno.h>
 #include <poll.h>
 #include <pwd.h>
+#include <sys/prctl.h>
+#include <sys/capability.h>
 #include <sys/stat.h>
-#include <linux/prctl.h>
 #include <cutils/sockets.h>
 #include <telephony/ril.h>
 //#include <media/AudioSystem.h>
@@ -208,16 +209,16 @@ switch(request) {
  */
 void switchUser()
 {
-//  prctl(PR_SET_KEEPCAPS, 1, 0, 0, 0);
-//  setuid(1001);
-// 
-//  struct __user_cap_header_struct header;
-//  struct __user_cap_data_struct cap;
-//  header.version = _LINUX_CAPABILITY_VERSION;
-//  header.pid = 0;
-//  cap.effective = cap.permitted = 1 << CAP_NET_ADMIN;
-//  cap.inheritable = 0;
-//  capset(&header, &cap);
+  prctl(PR_SET_KEEPCAPS, 1, 0, 0, 0);
+  setuid(1001);
+
+  struct __user_cap_header_struct header;
+  struct __user_cap_data_struct cap;
+  header.version = _LINUX_CAPABILITY_VERSION;
+  header.pid = 0;
+  cap.effective = cap.permitted = 1 << CAP_NET_ADMIN;
+  cap.inheritable = 0;
+  capset(&header, &cap);
 }
 
 static int
