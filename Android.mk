dab5c10047e132451cd336e79dea6f7f89dada02
Volker Mische <volker.mische@gmail.com>
2017-06-01 02:29:55 +0200

    Move `rilproxy.c` into root directory

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES:= src/rilproxy.c
+LOCAL_SRC_FILES:= rilproxy.c
 LOCAL_SHARED_LIBRARIES := libcutils liblog
 LOCAL_MODULE:= rilproxy
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
