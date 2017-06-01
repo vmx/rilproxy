5017201e3a2724b2db45ec6cd5fcfed02dbf4860
Volker Mische <volker.mische@gmail.com>
2017-06-01 02:59:27 +0200

    Clean up include file search paths
    Only `system/core/include` is needed.

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ AOSP_ROOT := /home/vmx/src/c/b2g/asop
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= rilproxy.c
-LOCAL_C_INCLUDES := "$(AOSP_ROOT)/system/core/include/" "$(AOSP_ROOT)/hardware/ril/include" "$(AOSP_ROOT)/frameworks/base/include" "${NDK}/sources/cxx-stl/system/include/" "$(AOSP_ROOT)/frameworks/av/include" "$(AOSP_ROOT)/hardware/libhardware/include" "$(AOSP_ROOT)/system/media/audio/include" "$(AOSP_ROOT)/frameworks/native/include"
+LOCAL_C_INCLUDES := "$(AOSP_ROOT)/system/core/include/"
 LOCAL_LDLIBS := -llog -lcutils -L"/home/vmx/src/c/b2g/asop/out/target/product/generic/system/lib"
 #LOCAL_LDLIBS := -llog -lcutils
 #LOCAL_LDFLAGS := -L/home/vmx/src/c/b2g/asop/out/target/product/generic/obj/STATIC_LIBRARIES/libcutils_intermediates
