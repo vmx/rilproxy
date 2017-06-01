580c496995e7ea04fb73c2ab79e09923ce48e57e
Volker Mische <volker.mische@gmail.com>
2017-06-01 03:03:58 +0200

    Cleanup `Android.mk` a bit

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,10 +4,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= rilproxy.c
 LOCAL_C_INCLUDES := "$(AOSP_ROOT)/system/core/include/"
-LOCAL_LDLIBS := -llog -lcutils -L"/home/vmx/src/c/b2g/asop/out/target/product/generic/system/lib"
-#LOCAL_LDLIBS := -llog -lcutils
-#LOCAL_LDFLAGS := -L/home/vmx/src/c/b2g/asop/out/target/product/generic/obj/STATIC_LIBRARIES/libcutils_intermediates
-#LOCAL_SHARED_LIBRARIES := libcutils liblog
+LOCAL_LDLIBS := -llog -lcutils -L"$(AOSP_ROOT)/out/target/product/generic/system/lib"
 LOCAL_MODULE:= rilproxy
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_MODULE_TAGS := eng
