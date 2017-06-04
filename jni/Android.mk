aa2863a4d968452655335c7750e96a593068e072
Volker Mische <volker.mische@gmail.com>
2017-06-04 19:03:24 +0200

    Make it work on x86

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,8 +1,16 @@
+ifeq ($(TARGET_ARCH_ABI),armeabi)
+    STATIC_LIB_PATH := "$(AOSP_ROOT)/out/target/product/generic/system/lib"
+else
+    ifeq ($(TARGET_ARCH_ABI),x86)
+        STATIC_LIB_PATH := "$(AOSP_ROOT)/out/target/product/generic_x86/system/lib"
+    endif
+endif
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= rilproxy.c
 LOCAL_C_INCLUDES := "$(AOSP_ROOT)/system/core/include/"
-LOCAL_LDLIBS := -llog -lcutils -L"$(AOSP_ROOT)/out/target/product/generic/system/lib"
+LOCAL_LDLIBS := -llog -lcutils -L$(STATIC_LIB_PATH)
 LOCAL_MODULE:= rilproxy
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_MODULE_TAGS := eng
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,2 @@
-APP_ABI := armeabi
+APP_ABI := x86 #armeabi
 APP_PLATFORM := 9
